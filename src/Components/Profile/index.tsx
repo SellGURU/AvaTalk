1e4395cf2f3b9a86ab5cf6f4e891f5c08f91258c
minasoheilifar <153756956+minasoheilifar@users.noreply.github.com>
2024-01-22 12:47:14 +0330

    profile responsive

--- src/Components/Profile/index.tsx
+++ src/Components/Profile/index.tsx
@@ -10,9 +10,10 @@ const Profile: React.FC<ProfileProps> = ({theme}) => {
   return (
     <>
     <div className={`${theme}-Profile-Container`}>
-      <div className="overflow-auto">
+      <div className={`${theme}-Profile-ProfileSection`}>
         <img className={`${theme}-Profile-Background`} src='./profile-background.png' />
         <div className={`${theme}-Profile-Content`}>
+          
           <div className="w-48">
             <Button theme="Carbon-Google">
               <img className="mr-2" src="./eye.svg" alt="" />
@@ -21,25 +22,29 @@ const Profile: React.FC<ProfileProps> = ({theme}) => {
               </div>
             </Button>
           </div>
+
           <div className={`${theme}-Profile-ProfilePicture`}>
             <div className={`${theme}-Profile-ProfilePictureBorder`}></div>
+            <div className={`${theme}-Profile-GalleryIcon`}>
+              <img src="./gallery-edit.svg" alt="" />
+            </div>
+            <div className={`${theme}-Profile-GalleryIcon ${theme}-Profile-GalleryImport`}>
+              <img src="./gallery-import.svg" alt="" />
+            </div>
           </div>
-          <div className={`${theme}-Profile-GalleryIcon`}>
-            <img src="./gallery-edit.svg" alt="" />
-          </div>
-          <div className={`${theme}-Profile-GalleryIcon ${theme}-Profile-GalleryImport`}>
-            <img src="./gallery-import.svg" alt="" />
-          </div>
+
           <div>
             <h1 className={`${theme}-Profile-ProfileName`}>Farzin Azami</h1>
             <p className={`${theme}-Profile-SubTitle`}>CoFounder & CEO</p>
           </div>
+
           <Button theme="Carbon">
             <img className="mr-2" src="./user-edit.svg" alt="" />
             <div>
               Edit Profile
             </div>
           </Button>
+
           <ContentCard theme="Carbon" title="Social">
             <div className={`${theme}-Profile-Icons`}>
               <div className={`${theme}-Profile-BackgroundIcons`}>
@@ -63,8 +68,8 @@ const Profile: React.FC<ProfileProps> = ({theme}) => {
           <ContentCard theme="Carbon" title="About me">
             <h1>Creating has always been fascinating to me and I have found it in design. As a designer, I am always trying to create or improve a more useful and purposeful user experience to make it more profitable for businesses.</h1>
           </ContentCard>
-        <Footer theme="Carbon"/>
         </div>
+        <Footer activeItem='' theme="Carbon"/>
       </div>
     </div>
     </>
