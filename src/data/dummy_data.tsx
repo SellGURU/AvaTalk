1a6b281d8a6d9ded1a3d334c7c9ba477500c208a
alirezarmj <a.rashidi.m78@gmail.com>
2024-01-25 10:50:54 +0330

    ContactList added

---
+++ src/data/dummy_data.tsx
@@ -0,0 +1,44 @@
+const dummyData = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john.doe@example.com",
+    image: "../../public/person.svg",
+    Exhibition: true,
+    Exchange: true,
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    image: "../../public/person.svg",
+    Exhibition: false,
+    Exchange: true,
+  },
+  {
+    id: 3,
+    name: "Bob Johnson",
+    email: "bob.johnson@example.com",
+    image: "../../public/person.svg",
+    Exhibition: true,
+    Exchange: false,
+  },
+  {
+    id: 4,
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    image: "../../public/person.svg",
+    Exhibition: false,
+    Exchange: false,
+  },
+  {
+    id: 5,
+    name: "Bob Johnson",
+    email: "bob.johnson@example.com",
+    image: "../../public/person.svg",
+    Exhibition: true,
+    Exchange: true,
+  },
+];
+
+export default dummyData;
