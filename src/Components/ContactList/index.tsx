cf85551dd2a1edfad96090ba32776781a339b918
amir <amirdehghani137722@gmail.com>
2024-01-26 18:59:33 +0330

    fix height issues

--- src/Components/ContactList/index.tsx
+++ src/Components/ContactList/index.tsx
@@ -1,5 +1,5 @@
 // import dummyData from "../../data/dummy_data";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import ContactItem from "../ContactItem";
 
 // interface Props {
@@ -14,25 +14,25 @@ interface DataProps {
   Exchange: boolean;
 }
 const ContactList = ({ data, theme }: { data: DataProps[]; theme: string | undefined }) => {
-  const [boxHeight, setBoxHeight] = useState(window.innerHeight);
-  const [variable, setVariable] = useState(boxHeight > 700 ? 0.63 : boxHeight > 650 ? 0.6 : boxHeight > 600 ? 0.55 : boxHeight > 530 ? 0.5 : 0.45);
-  const handleResize = () => {
-    setBoxHeight(window.innerHeight);
-    setVariable(window.innerHeight > 700 ? 0.63 : window.innerHeight > 650 ? 0.6 : window.innerHeight > 600 ? 0.55 : window.innerHeight > 530 ? 0.5 : 0.45);
-  };
-  useEffect(() => {
-    setBoxHeight(window.innerHeight);
-    setVariable(window.innerHeight > 700 ? 0.63 : window.innerHeight > 650 ? 0.6 : window.innerHeight > 600 ? 0.55 : window.innerHeight > 530 ? 0.5 : 0.45);
-    window.addEventListener("resize", handleResize, false);
+  // const [boxHeight, setBoxHeight] = useState(window.innerHeight);
+  // const [variable, setVariable] = useState(boxHeight > 700 ? 0.63 : boxHeight > 650 ? 0.6 : boxHeight > 600 ? 0.55 : boxHeight > 530 ? 0.5 : 0.45);
+  // const handleResize = () => {
+  //   setBoxHeight(window.innerHeight);
+  //   setVariable(window.innerHeight > 700 ? 0.63 : window.innerHeight > 650 ? 0.6 : window.innerHeight > 600 ? 0.55 : window.innerHeight > 530 ? 0.5 : 0.45);
+  // };
+  // useEffect(() => {
+  //   setBoxHeight(window.innerHeight);
+  //   setVariable(window.innerHeight > 700 ? 0.63 : window.innerHeight > 650 ? 0.6 : window.innerHeight > 600 ? 0.55 : window.innerHeight > 530 ? 0.5 : 0.45);
+  //   window.addEventListener("resize", handleResize, false);
 
-    return () => {
-      window.removeEventListener("resize", handleResize, false);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize, false);
+  //   };
+  // }, []);
 
   // console.log("boxHeight", boxHeight);
   return (
-    <div className="  w-full hiddenScrollBar mt-4" style={{ overflowY: "scroll", height: boxHeight * variable }}>
+    <div className=" w-full overflow-y-scroll hiddenScrollBar mt-4 h-[-webkit-fill-available]">
       {data.map((items, index) => (
         <ContactItem theme={theme} key={index} data={items} />
       ))}
