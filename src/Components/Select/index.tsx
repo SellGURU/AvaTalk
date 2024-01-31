56e674891d869b7b7d1afb4d1a9896d80946d282
alirezarmj <a.rashidi.m78@gmail.com>
2024-01-31 14:10:29 +0330

    Select Component modified

--- src/Components/Select/index.tsx
+++ src/Components/Select/index.tsx
@@ -35,19 +35,14 @@ const Select: React.FC<SelectProps> = ({ children, theme, label, valueElement, p
         </label>
         <div data-testid="input-container" data-showSelect={showSelect ? "true" : "false"} className={` w-[100%] ${theme}-Select-box `}>
           <div data-testid="input-id" data-showSelect={showSelect ? "true" : "false"} deta-selectBox="true" {...props} className={`${theme}-Select-input flex items-center`} id={inputId()}>
-            {valueElement ? (
-              updatedValueElement?.exhibition ? (
-                <div className="flex z-[100] exhibitionBoxShadows items-center justify-center w-[108px] space-x-[2px] h-8 rounded-[47px] bg-amber-400 ">
-                  <p className="text-gray-700 leading-[20px] text-[14px] font-[500] tracking-tight">Exhibition</p>
-                  <div onClick={handleCrossIconClick} className={` ${theme}-ContactDetails-crossIcon  `}></div>
-                </div>
-              ) : (
-                <span>{/* Your content for other cases is here */}</span>
-              )
+            {updatedValueElement?.exhibition ? (
+              <div className="flex z-[100] exhibitionBoxShadows items-center justify-center w-[108px] space-x-[2px] h-8 rounded-[47px] bg-amber-400 ">
+                <p className="text-gray-700 leading-[20px] text-[14px] font-[500] tracking-tight">Exhibition</p>
+                <div onClick={handleCrossIconClick} className={` ${theme}-ContactDetails-crossIcon  `}></div>
+              </div>
             ) : (
               <span className={`${theme}-Select-placeHolder`}>{placeholder}</span>
             )}
-            {/* {valueElement ? valueElement : <span className={`${theme}-Select-placeHolder`}>{placeholder}</span>} */}
           </div>
           <div
             onClick={() => {
@@ -64,49 +59,3 @@ const Select: React.FC<SelectProps> = ({ children, theme, label, valueElement, p
 };
 
 export default Select;
-
-/*
-import { HtmlHTMLAttributes, useEffect, useState } from "react";
-
-interface SelectProps extends HtmlHTMLAttributes<HTMLDivElement> {
-  theme?: string;
-  label?: string;
-  valueElement: React.ReactNode;
-  placeholder?: string;
-}
-function inputId(): string {
-  return "Select" + Math.floor(Math.random() * 100000).toString();
-}
-
-const Select: React.FC<SelectProps> = ({ children, theme, label, valueElement, ...props }) => {
-  const [showSelect, setShowSelect] = useState(false);
-  useEffect(() => {
-    setShowSelect(false);
-  }, [valueElement]);
-  return (
-    <>
-      <div className={`${theme}-Select-container w-[100%]`}>
-        <label className={`${theme}-Select-label`} htmlFor={inputId()}>
-          {label}
-        </label>
-        <div
-          data-testid="input-container"
-          data-showSelect={showSelect ? "true" : "false"}
-          onClick={() => {
-            setShowSelect(!showSelect);
-          }}
-          className={` w-[100%] ${theme}-Select-box `}
-        >
-          <div data-testid="input-id" data-showSelect={showSelect ? "true" : "false"} deta-selectBox="true" {...props} className={`${theme}-Select-input`} id={inputId()}>
-            {valueElement}
-          </div>
-          <div className={`${theme}-Select-icon `} style={{ rotate: showSelect == true ? "180deg" : "0deg" }}></div>
-        </div>
-        {showSelect && <div className={`${theme}-Select-dropDown-container`}>{children}</div>}
-      </div>
-    </>
-  );
-};
-
-export default Select;
-*/
