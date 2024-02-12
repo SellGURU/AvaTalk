e76ac603fcabb05bd7c83d6b7fabb44359f439cb
amir <amirdehghani137722@gmail.com>
2024-02-12 18:45:29 +0330

    fix setting issues

--- src/Pages/SettingPages/ConnectedAccount/index.tsx
+++ src/Pages/SettingPages/ConnectedAccount/index.tsx
@@ -1,39 +1,7 @@
-import { Button, TextField } from "symphony-ui"
-import { BackIcon,TextArea } from "../../../Components"
-import * as Yup from "yup";
-import { useFormik } from "formik";
-import { useAuth } from "../../../hooks/useAuth";
-import { AboutBox } from "../../../Model";
-import { useNavigate } from "react-router";
-
-
-
-const validationSchema = Yup.object().shape({
-    title:Yup.string().required(),
-    bio:Yup.string().required()
-});
-
+import { Button } from "symphony-ui"
+import { BackIcon, TextArea, TextField } from "../../../Components"
 const SettingConnectedAccount =() => {
-    const auth = useAuth()
-     const navigate = useNavigate();
-    const currentBox = auth.currentUser.boxs.filter((item) => item.getTypeName() == 'AboutBox')[0] as AboutBox
-    const initialValue = {
-        title:currentBox.getTitle(),
-        bio:currentBox.getBio()
-    };    
-    const formik = useFormik({
-        initialValues: initialValue,
-        validationSchema,
-        onSubmit: (values) => {
-        console.log(values);
-        },
-    });   
-    const submit = () => {
-        auth.currentUser.addBox(
-            new AboutBox(formik.values.title,formik.values.bio)
-        )
-        navigate('/')
-    }
+
     return (
         <>
             <div className="absolute w-full hiddenScrollBar h-dvh top-[0px] bg-white z-[15]">
@@ -42,15 +10,15 @@ const SettingConnectedAccount =() => {
 
                 </div>
                 <div className="mt-[120px] hiddenScrollBar h-full">
-                    {/* <div className="mt-24 px-6 text-left">
-                        <TextField {...formik.getFieldProps("title")} name="title" errorMessage={formik.errors?.title} theme="Carbon" label="Title" inValid={formik.errors?.title != undefined && (formik.touched?.title as boolean)}  type="text" placeholder="Enter title..."></TextField>
+                    <div className="mt-24 px-6 text-left">
+                        <TextField onBlur={() => {}} onChange={() =>{}}  value="" name="title"  theme="Carbon" label="Title" inValid={false}  type="text" placeholder="Enter title..."></TextField>
                     </div>
                     <div className="px-6 mt-3 text-left">
-                        <TextArea  {...formik.getFieldProps("bio")} errorMessage={formik.errors?.bio} placeholder="Write your bio ..." inValid={formik.errors?.bio != undefined && (formik.touched?.bio as boolean)} textAreaHeight="136px" theme="Carbon" label="Bio" name="bio" ></TextArea>
+                        <TextArea inValid={false} onBlur={() => { } } onChange={() => { } } placeholder="Write your bio ..." textAreaHeight="136px" theme="Carbon" label="Bio" name="bio" value={""} ></TextArea>
                     </div>
                     <div className="px-6 mt-10">
-                        <Button onClick={submit} theme="Carbon">Save Change</Button>
-                    </div> */}
+                        <Button  theme="Carbon">Save Change</Button>
+                    </div>
 
                 </div>
             </div>
