c6792c71d6884a847111b0f6fb7283dd4eb8b0b8
alirezarmj <a.rashidi.m78@gmail.com>
2024-02-13 16:44:49 +0330

    made charts responsive

--- src/Components/AreaChartComponent/index.tsx
+++ src/Components/AreaChartComponent/index.tsx
@@ -1,6 +1,4 @@
-// import React, { PureComponent } from "react";
-
-import { Area, XAxis, YAxis, CartesianGrid, Tooltip, AreaChart } from "recharts";
+import { Area, XAxis, YAxis, CartesianGrid, Tooltip, AreaChart, ResponsiveContainer } from "recharts";
 
 const data = [
   {
@@ -55,27 +53,25 @@ const data = [
 
 const AreaChartComponent = () => {
   return (
-    <div className=" rounded-[24px] bg-gray-100 pt-10 pb-4 boxShadow-Gray ">
-      {/* <ResponsiveContainer width="100%" height="100%"> */}
-      <p className=" text-gray-700 leading-[21px] text-[14px] font-[500] pl-16 pb-2">Page Views</p>
-      <AreaChart
-        width={500}
-        height={400}
-        data={data}
-        // margin={{
-        //   top: 10,
-        //   right: 30,
-        //   left: 0,
-        //   bottom: 0,
-        // }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" strokeWidth="0px" fontSize={12} stroke="#9CA3AF" />
-        <YAxis fontSize={12} stroke="#9CA3AF" />
-        <Tooltip />
-        <Area type="monotone" dataKey="view" stroke="#8884d8" fill="#8884d8" />
-      </AreaChart>
-      {/* </ResponsiveContainer> */}
+    <div className="rounded-[24px] bg-gray-100 pt-10 pb-4 boxShadow-Gray">
+      <p className="text-gray-700 leading-[21px] text-[14px] font-[500] pl-16 pb-2">Page Views</p>
+      <ResponsiveContainer width="100%" height={400}>
+        <AreaChart
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" strokeWidth="0px" fontSize={12} stroke="#9CA3AF" />
+          <YAxis fontSize={12} stroke="#9CA3AF" />
+          <Tooltip />
+          <Area type="monotone" dataKey="view" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   );
 };
