217935ab8809a383d5540512a06400e0421a8a90
amir <amirdehghani137722@gmail.com>
2024-02-14 08:43:06 +0330

    fix some layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   useMoch()
   return (
     <>
-      <div className='w-full h-dvh flex justify-center items-center '>
+      <div className='w-full h-dvh flex overflow-hidden justify-center items-center '>
         <div className='w-full max-w-xl'>
           <RouterProvider router={route} />
           <ToastContainer />
