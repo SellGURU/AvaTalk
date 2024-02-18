76de9f984982c84bd993bcabd7ded3caff525438
alirezarmj <a.rashidi.m78@gmail.com>
2024-02-18 09:59:58 +0330

    Add Contact lagic Built

--- src/Components/__Modal__/AddContact/index.tsx
+++ src/Components/__Modal__/AddContact/index.tsx
@@ -7,24 +7,45 @@ import { useState } from "react";
 import LocationPicker from "react-leaflet-location-picker";
 import { SearchBox, Select, TextArea, TextField } from "../..";
 import { Button } from "symphony-ui";
+import { generateSlugId } from "../../../help";
+import { Tag } from "../../../Types";
+import { useAuth } from "../../../hooks/useAuth";
+import { GoogleMapBox } from "../../../Model";
+
+interface ContactType {
+  id: string;
+  fullName: string;
+  email: string;
+  phone: string;
+  company: string;
+  job: string;
+  mapLocation: { lat: number | undefined; lng: number | undefined };
+  tags: Tag[];
+  note?: string;
+}
 
 interface AddContactProps {
   isOpen: boolean;
   onClose: () => void;
   theme?: string;
   onAfterOpen?: () => void;
+  onAddContact: (formData: ContactType) => void;
 }
 
-const AddContact: React.FC<AddContactProps> = ({ isOpen, onAfterOpen, onClose, theme }) => {
-  // const [location, setLocation] = useState({
-  //   lat: 51.5072,
-  //   lng: 0.1276,
-  // });
-  const location = {
-    lat: 51.5072,
-    lng: 0.1276,
-  };
-  const [pointVals, setPointVals] = useState([[location.lat, location.lng]]);
+const AddContact: React.FC<AddContactProps> = ({ isOpen, onAfterOpen, onClose, theme, onAddContact }) => {
+  const auth = useAuth();
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    phone: "",
+    company: "",
+    mapLocation: { lat: 0, lng: 0 },
+    job: "",
+    tags: [],
+    note: "",
+  });
+
+  const [pointVals, setPointVals] = useState([[auth.currentUser.information?.location.lat, auth.currentUser.information?.location.lng]]);
   const pointMode = {
     banner: false,
     control: {
@@ -33,10 +54,47 @@ const AddContact: React.FC<AddContactProps> = ({ isOpen, onAfterOpen, onClose, t
       onRemove: (point: any) => console.log("I've just been clicked for removal :(", point),
     },
   };
+  let currentBox = auth.currentUser.boxs.filter((item) => item.getTypeName() == "GoogleMapBox")[0] as GoogleMapBox;
+  if (currentBox == undefined) {
+    currentBox = new GoogleMapBox("", {
+      lat: auth.currentUser.information?.location.lat as number,
+      lan: auth.currentUser.information?.location.lng as number,
+    });
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleAddContact = () => {
+    const id = generateSlugId();
+
+    const formDataWithId = { ...formData, id, mapLocation: { lat: pointVals[0][1], lng: pointVals[0][0] } }; // Add id to formData
+
+    onAddContact(formDataWithId);
+    console.log(formDataWithId);
+
+    setFormData({
+      fullName: "",
+      email: "",
+      phone: "",
+      company: "",
+      mapLocation: { lat: 0, lng: 0 },
+      job: "",
+      tags: [],
+      note: "",
+    });
+    onClose();
+  };
+
   const [country, setCountry] = useState<any>({
     codeName: "us",
     codePhone: "+1",
   });
+
   return (
     <>
       <Modal
@@ -48,11 +106,7 @@ const AddContact: React.FC<AddContactProps> = ({ isOpen, onAfterOpen, onClose, t
       >
         <>
           <div className="h-[80vh] hiddenScrollBar overflow-y-scroll">
-            <div className="">
-              {/* <div className="w-full flex justify-center items-center mt-2 mb-6">
-                <div className="text-sm text-gray-700 max-w-[228px] opacity-80 text-center">Tell us more about yourself</div>
-              </div> */}
-            </div>
+            <div className=""></div>
             <div className="p-5">
               <div className="flex justify-between items-center">
                 <div className="text-gray-700 text-left font-[600] text-[16px] leading-[24px]">Add Contact</div>
@@ -62,13 +116,13 @@ const AddContact: React.FC<AddContactProps> = ({ isOpen, onAfterOpen, onClose, t
               </div>
               <div className="my-4">
                 <TextField
-                  value=""
-                  onChange={() => {}}
+                  value={formData.fullName}
+                  onChange={handleInputChange}
                   onBlur={() => {}}
                   label="Full Name"
                   placeholder="Enter your first and last name..."
                   theme="Carbon"
-                  name="FullName"
+                  name="fullName"
                   type="text"
                   errorMessage=""
                   inValid={false}
@@ -77,13 +131,13 @@ const AddContact: React.FC<AddContactProps> = ({ isOpen, onAfterOpen, onClose, t
 
               <div className="mb-4">
                 <TextField
-                  value=""
-                  onChange={() => {}}
+                  value={formData.email}
+                  onChange={handleInputChange}
                   onBlur={() => {}}
                   label="Email Address"
                   placeholder="Enter your email address..."
                   theme="Carbon"
-                  name="EmailAddress"
+                  name="email"
                   type="text"
                   errorMessage=""
                   inValid={false}
@@ -92,13 +146,13 @@ const AddContact: React.FC<AddContactProps> = ({ isOpen, onAfterOpen, onClose, t
 
               <div className="mb-4">
                 <TextField
-                  value=""
-                  onChange={() => {}}
+                  value={formData.phone}
+                  onChange={handleInputChange}
                   onBlur={() => {}}
                   label="Phone"
                   placeholder="Enter your phone number..."
                   theme="Carbon"
-                  name="Phone"
+                  name="phone"
                   type="phone"
                   phoneCountry={country}
                   setPhoneCountry={setCountry}
@@ -114,13 +168,13 @@ const AddContact: React.FC<AddContactProps> = ({ isOpen, onAfterOpen, onClose, t
               </div>
               <div className="mt-4">
                 <TextField
-                  value=""
-                  onChange={() => {}}
+                  value={formData.company}
+                  onChange={handleInputChange}
                   onBlur={() => {}}
                   label="Company"
                   placeholder="Enter your company name..."
                   theme="Carbon"
-                  name="Phone"
+                  name="company"
                   type="text"
                   errorMessage=""
                   inValid=""
@@ -128,13 +182,13 @@ const AddContact: React.FC<AddContactProps> = ({ isOpen, onAfterOpen, onClose, t
               </div>
               <div className="mt-4">
                 <TextField
-                  value=""
-                  onChange={() => {}}
+                  value={formData.job}
+                  onChange={handleInputChange}
                   onBlur={() => {}}
                   label="Job Title"
                   placeholder="Enter your job title..."
                   theme="Carbon"
-                  name="Phone"
+                  name="job"
                   type="text"
                   errorMessage=""
                   inValid=""
@@ -142,19 +196,32 @@ const AddContact: React.FC<AddContactProps> = ({ isOpen, onAfterOpen, onClose, t
               </div>
 
               <div className="mt-4">
-                <Select valueElement={
+                <Select
+                  valueElement={
                     <div className={`cursor-pointer mt-[2px] flex justify-start items-center`}>
-                        <div className="ml-1 text-gray-700 text-sm">dis</div>
+                      <div className="ml-1 text-gray-700 text-sm">dis</div>
                     </div>
-                } label="Tag" placeholder="Select tag..." theme="Carbon" >
-                  
-                </Select>
+                  }
+                  label="Tag"
+                  placeholder="Select tag..."
+                  theme="Carbon"
+                ></Select>
               </div>
               <div className="mt-4">
-                <TextArea inValid="" placeholder="Enter your note..." textAreaHeight="136px" name="" onBlur={() => {}} label="Note" theme="Carbon" onChange={() => {}} value="" />
+                <TextArea
+                  inValid=""
+                  placeholder="Enter your note..."
+                  textAreaHeight="136px"
+                  name="note"
+                  onBlur={() => {}}
+                  label="Note"
+                  theme="Carbon"
+                  onChange={handleInputChange}
+                  value={formData.note}
+                />
               </div>
               <div className="mt-4">
-                <Button onClick={onClose} theme="Carbon">
+                <Button onClick={handleAddContact} theme="Carbon">
                   <div>Add Contact</div>
                 </Button>
               </div>
--- src/Types/index.ts
+++ src/Types/index.ts
@@ -1,46 +1,39 @@
 interface PhoneCountry {
-  codeName : string;
-  codePhone: string
+  codeName: string;
+  codePhone: string;
 }
 
 interface Tag {
-  id:string;
-  name:string;
-  color:string;
-  contacts:number;
+  id: string;
+  name: string;
+  color: string;
+  contacts: number;
 }
 
 interface Contact {
   id: string;
-  firstName:string
-  lastName: string;
+  fullName: string;
   email: string;
-  photo: string;
-  tags:Array<Tag>
-  isExchange: boolean;
+  photo?: string;
+  tags: Array<Tag>;
+  isExchange?: boolean;
   phone: string;
-  location: string;
+  location?: string;
+  mapLocation?: { lat: number | undefined; lng: number | undefined };
   company: string;
-  meetDate: string;
-  addDate: string;
+  meetDate?: string;
+  addDate?: string;
   job: string;
 }
 
 interface chat {
-  from:'Ai'|'user',
-  text:string,
-  currentconverationid:string,
-  instanceid:string,
-  audio_file:string
+  from: "Ai" | "user";
+  text: string;
+  currentconverationid: string;
+  instanceid: string;
+  audio_file: string;
 }
 
-type MenuType = 'profile'| 'contacts' | 'chats' | 'status' | 'settings';
+type MenuType = "profile" | "contacts" | "chats" | "status" | "settings";
 
-
-export type {
-    PhoneCountry,
-    MenuType,
-    Tag,
-    Contact,
-    chat
-}
+export type { PhoneCountry, MenuType, Tag, Contact, chat };
