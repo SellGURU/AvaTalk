67b49beddab9c63a0a10819de7eb6ffd94df9e62
amir <amirdehghani137722@gmail.com>
2024-04-02 14:14:19 +0330

    fix some iisues

--- src/Components/Profile/index.tsx
+++ src/Components/Profile/index.tsx
@@ -265,7 +265,7 @@ const Profile: React.FC<ProfileProps> = ({theme}) => {
                 :undefined}
               </div>
               <div className={`${(showToturial && toturialStep == 2) ? 'relative z-50  bg-white p-2 rounded-full ' :''}`}>
-                <Button onClick={() => {setShowBookMark(true)}} data-mode="calendar" theme='Carbon-back'>
+                <Button disabled onClick={() => {setShowBookMark(true)}} data-mode="calendar" theme='Carbon-back'>
                   <div className={`${theme}-Profile-CalenderBtnVector`}></div>
                 </Button>   
                 {(showToturial && toturialStep == 2) ?
@@ -305,7 +305,7 @@ const Profile: React.FC<ProfileProps> = ({theme}) => {
                 :undefined}                 
               </div>
                 <div className={`${(showToturial && toturialStep == 4) ? 'relative z-50  bg-white p-2 rounded-[20px] ' :''} w-full`}>
-                  <div className='borderBox-Gray boxShadow-Gray h-11 flex justify-center items-center rounded-[27px] text-gray-700 text-sm font-semibold cursor-pointer w-full' onClick={() => {setShowExchangeContact(true)}}>Exchange Contact</div>
+                  <div  className='borderBox-Gray opacity-50 cursor-not-allowed boxShadow-Gray h-11 flex justify-center items-center rounded-[27px] text-gray-700 text-sm font-semibold  w-full' onClick={() => {setShowExchangeContact(false)}}>Exchange Contact</div>
                   {(showToturial && toturialStep == 4) ?
                   <ToturialsBox theme='Carbon' left='-80' isLast position='top' skip={() => {
                     setShowToturial(false)
