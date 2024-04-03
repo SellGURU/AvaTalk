9eebb4e26581526befd61a82748b4d95a3d3b3e2
amir <amirdehghani137722@gmail.com>
2024-04-03 19:23:44 +0330

    fix share url

--- src/Components/Profile/index.tsx
+++ src/Components/Profile/index.tsx
@@ -342,7 +342,7 @@ const Profile: React.FC<ProfileProps> = ({theme}) => {
       {mode == 'review' || mode == 'share' ?
         <div className=''>
           <Button onClick={() => {
-            window.open('https://ar.avatalk.me/#detect1/?user'+shareUser.information?.userId)
+            window.open('https://ar.avatalk.me/#detect1/?user='+shareUser.information?.userId)
           }} data-mode={(showToturial && toturialStep == 1)?"toturials":''} theme='Carbon-Ar'>
             <div>
               <div className={`${theme}-Ar-Button-icon`}></div>
