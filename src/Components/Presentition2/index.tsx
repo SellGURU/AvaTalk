ae94aa96b786b2df12f44de10a611d5ef7a50481
minasoheilifar <mina.soheilifar@gmail.com>
2024-04-15 15:53:22 +0330

    presentation

--- src/Components/Presentition2/index.tsx
+++ src/Components/Presentition2/index.tsx
@@ -1,8 +1,9 @@
-import { useRef, useState } from "react"
+import { useRef, useState ,useEffect } from "react"
 import FooterPresentation from "../FooterPresentation"
 import { sendToApi } from "../../help";
 import { chat } from "../../Types";
 import { User } from "../../Model";
+import { Button } from "symphony-ui";
 
 const Presentition2 = () => {
     const languagesList = [
@@ -26,6 +27,16 @@ const Presentition2 = () => {
     const [showSuggestions,setShowSuggestions] = useState(false);     
     const [shareUser,setShareUser] = useState<null | User>(null)
     const BLokedIdList =useRef<string[]>([]);
+    const [showContent, setShowContent] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowContent(true);
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     const handleSendVector = (value: string) => {
         setShowSuggestions(false)
         setIsLoading(true)
@@ -39,6 +50,14 @@ const Presentition2 = () => {
     };     
     return (
         <>
+        {showContent && (
+            <div className="w-full px-5 pt-5 pb-3 flex flex-col gap-3">
+                <p className="font-semibold">Ask me more information</p>
+                <Button theme="Carbon">Can you introduce yourself?</Button>
+                <Button theme="Carbon">Tell me more about your business</Button>
+                <Button theme="Carbon">What services do you provide in Codie?</Button>
+            </div>
+        )}
         <FooterPresentation langCode={selectedLang.code} isRecording={isRecording} setIsRecording={setIsRecording} isLoading={isLoading} theme="Carbon" onSendVector={handleSendVector}/>
         </>
     )
