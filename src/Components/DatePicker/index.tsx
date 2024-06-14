def436b3b74302f731655b482f5f7d49c1e1f0ed
amir <amirdehghani137722@gmail.com>
2024-06-14 13:11:36 +0330

    fix analiyz

--- src/Components/DatePicker/index.tsx
+++ src/Components/DatePicker/index.tsx
@@ -12,38 +12,41 @@ const DatePicker = (props:any) => {
 
   return (
     <div className="flex mt-4 space-x-3  items-center ">
-      <div className=" h-[44px] flex justify-center rounded-[21px] w-4/6 borderBox-GrayBox boxShadow-Gray ">
-        <div className="h-full flex items-center justify-start">
-        <div className={`Carbon-Profile-EditProfileBtnVector6 Carbon-Footer-Vectors
-                         text-[#8290a3] w-6 h-6 ms-2
-                        `}></div>
-          {/* <img src="../../../Carbon/Calendar-new.svg" className="w-6 h-6 ms-2 " alt="" /> */}
-          <Litepicker
-            className="text-gray-700 text-sm w-56 pl-1 bg-inherit cursor-pointer"
-            value={`${props.day.startDate.toLocaleDateString()} - ${props.day.endDate.toLocaleDateString()}`}
-            onChange={(value) => {
-              console.log(value)
-              const [startDateString, endDateString] = value.split(" - ");
-              props.setDay({
-                startDate: new Date(startDateString),
-                endDate: new Date(endDateString),
-              });
-            }}
-            options={{
-              autoApply: false,
-              format: "YYYY-MM-DD",
-              singleMode: false,
-              numberOfColumns: 1,
-              numberOfMonths: 2,
-              showWeekNumbers: true,
-              dropdowns: {
-                minYear: 1990,
-                maxYear: null,
-                months: true,
-                years: true,
-              },
-            }}
-          />
+      <div className=" h-[44px] flex justify-between rounded-[21px] w-4/6 borderBox-GrayBox boxShadow-Gray ">
+        <div className="h-full flex items-center justify-between w-full px-2">
+       <div className=" flex justify-start items-center">
+          <div className={`Carbon-Profile-EditProfileBtnVector6  px-3 Carbon-Footer-Vectors
+                          text-[#8290a3] w-6 h-6 ms-2
+                          `}></div>
+            {/* <img src="../../../Carbon/Calendar-new.svg" className="w-6 h-6 ms-2 " alt="" /> */}
+            <Litepicker
+              className="text-gray-700 text-sm w-40 pl-1 bg-inherit cursor-pointer"
+              value={`${props.day.startDate.toLocaleDateString()} - ${props.day.endDate.toLocaleDateString()}`}
+              onChange={(value) => {
+                console.log(value)
+                const [startDateString, endDateString] = value.split(" - ");
+                props.setDay({
+                  startDate: new Date(startDateString),
+                  endDate: new Date(endDateString),
+                });
+              }}
+              options={{
+                autoApply: false,
+                format: "YYYY-MM-DD",
+                singleMode: false,
+                numberOfColumns: 1,
+                numberOfMonths: 2,
+                showWeekNumbers: true,
+                dropdowns: {
+                  minYear: 1990,
+                  maxYear: null,
+                  months: true,
+                  years: true,
+                },
+              }}
+            />
+
+       </div>
           <img src="../../../Carbon/leftVector.svg" className="w-4 h-4 me-2 -rotate-90 " alt="" />
         </div>
 
