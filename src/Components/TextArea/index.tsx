aa7f6809dbda30504ffb4325d214dd52d9492036
amir <amirdehghani137722@gmail.com>
2024-08-27 17:28:09 +0330

    update ai setting

--- src/Components/TextArea/index.tsx
+++ src/Components/TextArea/index.tsx
@@ -1,4 +1,5 @@
-import React, { TextareaHTMLAttributes } from "react";
+import React, { TextareaHTMLAttributes, useState } from "react";
+import { BeatLoader } from "react-spinners";
 
 // import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
@@ -9,12 +10,14 @@ interface InputProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   value: string;
   textAreaHeight?: string;
   required?: boolean;
+  isAnalyse?:boolean;
   placeholder?: string;
   onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
   onBlur: (event: React.FocusEvent<HTMLTextAreaElement>) => void;
   //   type: "text" | "password" | "email";
   inValid: boolean | string;
   errorMessage?: string;
+  setShowSuggestion?:(action:boolean) => void
 }
 
 function inputId(): string {
@@ -25,6 +28,7 @@ const TextArea: React.FC<InputProps> = ({
   theme,
   label,
   name,
+  isAnalyse,
   //   type,
   placeholder,
   textAreaHeight,
@@ -33,10 +37,19 @@ const TextArea: React.FC<InputProps> = ({
   onBlur,
   value,
   inValid,
-
+  setShowSuggestion,
   errorMessage,
   ...props
 }) => {
+  const [isAnalysing,setIsAnalysing] = useState(false)
+  const [isCompleteAnalyse,setIsCompleteAnalyse] = useState(false)
+  const analyseText = () => {
+    setIsAnalysing(true)
+    setTimeout(() => {
+      setIsAnalysing(false)
+      setIsCompleteAnalyse(true)
+    },3000);
+  }
   return (
     <div className={`${theme}-TextArea-container w-[100%]`}>
       <label className={`${theme}-TextArea-label`} htmlFor={inputId()}>
@@ -47,6 +60,7 @@ const TextArea: React.FC<InputProps> = ({
         <textarea
           data-testid="input-id"
           {...props}
+          data-mode={isAnalyse?'analyse':''}
           // className={`${theme}-TextArea-input w-[100%] `}
           className={`${theme}-TextArea-input`}
           //   type={getInputType()}
@@ -54,10 +68,48 @@ const TextArea: React.FC<InputProps> = ({
           style={{ height: textAreaHeight }}
           placeholder={placeholder}
           name={name}
+          disabled={isAnalysing}
           onChange={onChange}
           onBlur={onBlur}
           value={value}
         />
+        {isAnalyse && 
+        <>  
+          {isAnalysing &&
+            <>
+              <div className="absolute bottom-1 right-5">
+                <BeatLoader color="#5B21B6" size={8}></BeatLoader>
+              </div>
+            </>
+          }
+          {
+            !isAnalysing && !isCompleteAnalyse &&
+              <div onClick={() => {
+                analyseText()
+              }} className="absolute bg-gray-100 flex py-2 px-1 justify-end items-center bottom-0 right-5">
+                <img src="./Carbon/analyseStar.svg" alt="" />
+                <div className="text-primary-color cursor-pointer ml-1 text-sm font-medium">
+                  Analyze by AI
+                </div>
+              </div>
+          }
+          {
+            isCompleteAnalyse && 
+              <div onClick={() => {
+                // analyseText()
+                if(setShowSuggestion){
+                  setShowSuggestion(true)
+                }
+              }} className="absolute bg-gray-100 flex py-2 px-1 justify-end items-center bottom-0 right-5">
+                <img src="./Carbon/tick.svg" alt="" />
+                <div className="text-primary-color cursor-pointer ml-1 text-sm font-medium">
+                  Analyzed
+                </div>
+              </div>            
+          }
+        
+        </>
+        }
       </div>
       {inValid && <div className={`${theme}-TextArea-error`}>{errorMessage}</div>}
     </div>
