ea20a767a3a5ac41aa7a70d6ee27f2c03d7ede27
amir <amirdehghani137722@gmail.com>
2024-09-10 11:29:09 +0330

    add learn more

---
+++ src/Pages/EditPages/AiSetting/learnMore.ts
@@ -0,0 +1,16 @@
+export const learnMore = `
+Avatalk's AI Knowledge section is the hub for crafting a personalized and effective AI-driven persona. This feature ensures your digital interactions reflect your unique professional expertise, style, and preferences, enhancing your presence and effectiveness across virtual platforms.
+Purpose of Customizing Your AI Knowledge
+The core of Avatalk's appeal lies in its ability to let you tailor your AI's knowledge base, which in turn shapes how your digital persona interacts in professional settings. Whether it’s for virtual networking, engaging potential clients, or maintaining an active professional online presence, the information you feed into your AI helps it deliver insightful, relevant responses. This customization aligns your AI's interactions with your personal and professional ambitions, helping you stand out in a competitive digital marketplace.
+How to Use This Section
+Navigate to the "AI Settings" in Avatalk to start customizing:
+AI Knowledge Input: Use the rich text editor to enter details about your professional domain, expertise, and other pertinent information you wish your AI to communicate. The more detailed and specific your input, the more accurately and effectively your AI represents you.
+Voice Gender Selection: Choose your AI persona's voice gender from a dropdown menu to match your professional identity, reinforcing the consistency of your digital persona.
+Analyze by AI: This functionality analyzes your inputs and suggests enhancements to increase clarity and engagement, ensuring your AI persona communicates with sophistication and professionalism.
+Saving Changes: After tweaking your AI's knowledge and voice settings, click "Save Changes" to implement them. Your AI will instantly use this updated information, ensuring your digital presence remains polished and precise.
+Upgrading to the Pro Version
+While the free version of Avatalk offers substantial benefits, upgrading to the Pro version unlocks unlimited edits and continuous refinements of your AI persona. This allows for ongoing adjustments as your professional needs evolve, giving you the flexibility to keep your digital persona as dynamic and responsive as your career itself.
+Maximize Your AI Knowledge for Effective Networking
+With Avatalk’s intuitive design, you can readily update and refine your AI settings, ensuring your persona remains relevant and impa
+
+`
