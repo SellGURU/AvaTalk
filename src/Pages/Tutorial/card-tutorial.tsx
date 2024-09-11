436e9f1eb117811246b399b55a783e49b589a455
amin <aminshahini18@gmail.com>
2024-09-11 13:25:06 +0330

    FIXED:video rated ui fixed

--- src/Pages/Tutorial/card-tutorial.tsx
+++ src/Pages/Tutorial/card-tutorial.tsx
@@ -14,11 +14,23 @@ export const CartTu = ({link,total_views,videoId,rate}:Props) => {
 
     const [videoRate,setVideoRate]=useState<any>()
     const [isLoading,setIsLoading]=useState<boolean>(true);
-    const getData=async ()=>{
+    const svgImage= (<svg aria-hidden="true" className="rr--svg" xmlns="http://www.w3.org/2000/svg" viewBox="-1 -1 27 25.81"
+                    preserveAspectRatio="xMidYMid meet" stroke-width="2">
+        <g shape-rendering="geometricPrecision">
+            <polygon
+                points="25 9.02 16.4 7.75 12.46 0 8.59 7.79 0 9.14 6.21 15.23 4.85 23.81 12.55 19.79 20.3 23.74 18.85 15.17 25 9.02"></polygon>
+        </g>
+    </svg>)
+    const customStyles = {
+        itemShapes:svgImage,
+        activeFillColor: '#5B21B6',
+        inactiveFillColor: '#fff',
+    };
+    const getData = async () => {
         setIsLoading(true)
-        const videosRes=await TutorialApi.checkRating(videoId);
+        const videosRes = await TutorialApi.checkRating(videoId);
         setVideoRate(videosRes.data.rated)
-        console.log(typeof  videosRes.data.rated)
+        console.log(typeof videosRes.data.rated)
         setIsLoading(false);
     }
     useEffect(()=>{
@@ -55,6 +67,7 @@ export const CartTu = ({link,total_views,videoId,rate}:Props) => {
                     value={rating}
                     onChange={setRating}
                     readOnly={videoRate}
+                    itemStyles={customStyles}
                 />}
                 <p className={"text-[12px] font-normal"}>{total_views} reviews</p>
                 <p className={"text-[10px] font-normal"}>08/08/2024</p>
