5305ec9603423c6ed7b138b796a4ceea9f3ca102
amin <aminshahini18@gmail.com>
2024-09-11 12:55:29 +0330

    FIXED:star fixed

--- src/Pages/Tutorial/card-tutorial.tsx
+++ src/Pages/Tutorial/card-tutorial.tsx
@@ -1,5 +1,7 @@
 import {useEffect, useState} from "react";
 import {TutorialApi} from "../../Api";
+import {Rating} from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css';
 
 interface  Props{
     link:string,
@@ -7,7 +9,9 @@ interface  Props{
     videoId:string
 }
 export const CartTu = ({link,total_views,videoId}:Props) => {
-    const [videoRate,setVideoRate]=useState<[]>()
+    const [rating, setRating] = useState(3);
+
+    const [videoRate,setVideoRate]=useState<any>()
     const [isLoading,setIsLoading]=useState<boolean>(false);
     const getData=async ()=>{
         setIsLoading(true)
@@ -33,33 +37,13 @@ export const CartTu = ({link,total_views,videoId}:Props) => {
             <div className={"flex items-center w-full justify-between"}>
 
 
-                <div className="flex items-center ">
-                    <svg className="w-4 h-4 text-[#5B21B6] ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                         fill="currentColor" viewBox="0 0 22 20">
-                        <path
-                            d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                    </svg>
-                    <svg className="w-4 h-4 text-[#5B21B6] ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                         fill="currentColor" viewBox="0 0 22 20">
-                        <path
-                            d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                    </svg>
-                    <svg className="w-4 h-4 text-[#5B21B6] ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                         fill="currentColor" viewBox="0 0 22 20">
-                        <path
-                            d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                    </svg>
-                    <svg className="w-4 h-4 text-[#5B21B6] ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                         fill="currentColor" viewBox="0 0 22 20">
-                        <path
-                            d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                    </svg>
-                    <svg className="w-4 h-4 ms-1 text-white " aria-hidden="true"
-                         xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                        <path
-                            d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                    </svg>
-                </div>
+
+                <Rating
+                    className={"!w-24"}
+                    value={rating}
+                    onChange={setRating}
+                    readOnly={videoRate.rated}
+                />
                 <p className={"text-[12px] font-normal"}>{total_views} reviews</p>
                 <p className={"text-[10px] font-normal"}>08/08/2024</p>
 
