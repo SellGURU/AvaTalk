6ca1845f6862001086682bb22a77f9e36f838a21
amir <amirdehghani137722@gmail.com>
2024-09-16 12:50:11 +0330

    fix nfc card

--- src/Components/OrderNFC__Card/index.tsx
+++ src/Components/OrderNFC__Card/index.tsx
@@ -24,7 +24,7 @@ export const CardOrderNFCProduct = ({listColorBadge,title,price,imgSrc,id}:Props
                     <img src={imgSrc} className={"h-[115px] w-fit"}  alt={""}/>
                 </div>
                 <div className="p-4 bg-white mt-0 ">
-                    <h1 className={"text-[10px] h-10 text-[#374151] font-bold"}>{title}</h1>
+                    <h1 className={"text-[10px] h-[40px] text-[#374151] font-bold"}>{title}</h1>
                     <div className={"text-[10px] text-[#6B7280] flex gap-2 items-center"}>
                         Colors:
                         {listColorBadge.map((color:string,index) => (
