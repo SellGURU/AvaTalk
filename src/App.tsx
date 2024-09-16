1c5c4ca7524148c8c7990831cc761fa868b5aa2c
amir <amirdehghani137722@gmail.com>
2024-09-16 19:30:23 +0330

    fix width

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div className='w-full h-dvh flex overflow-hidden justify-center items-center '>
-        <div className='w-full max-w-xl'>
+        <div className='w-full max-w-[32rem]'>
             <GoogleOAuthProvider clientId="750278697489-u68emmire3d35234obo1mne9v0eobmsu.apps.googleusercontent.com">
               <RouterProvider router={route} />
               <ToastContainer />
