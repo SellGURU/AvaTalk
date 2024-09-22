983ea97ebccb374ad1dc8d27167f7268218fdcef
amir <amirdehghani137722@gmail.com>
2024-09-22 15:32:18 +0330

    fix  toogle button

--- src/Components/ToggleButton2/index.tsx
+++ src/Components/ToggleButton2/index.tsx
@@ -20,13 +20,19 @@ const ToggleButton2: React.FC<ToggleProps> = ({ theme,value, leftText, rightText
     <div className={`${theme}-ToggleButton-container px-2 `}>
       <div className={`${theme}-ToggleButton-notActiveButton w-full justify-center flex border border-solid border-transparent px-2   ${activeButton === leftText ? activeStyle : ""} `}>
         <Button data-mode={activeButton == leftText ?'active':""} onClick={() => handleButtonClick(leftText)} theme="Carbon-Toggle">
-          <img src="./Carbon/userVector.svg" className=" w-5 h-5 me-[6px]"/>
+          {
+            activeButton == leftText?
+            <img className=" w-5 h-5 me-[6px]" src="./Carbon/profile-circle.svg" />
+            :
+            <img src="./Carbon/userVector.svg" className=" w-5 h-5 me-[6px]"/>
+          }
           {leftText}
         </Button>
       </div>
       <div className={`${theme}-ToggleButton-notActiveButton w-full justify-center flex border border-solid border-transparent px-2  ${activeButton === rightText ? activeStyle : ""} `}>
         <Button data-mode={activeButton == rightText ?'active':""} onClick={() => handleButtonClick(rightText)} theme="Carbon-Toggle">
-          <img src="./Carbon/chatVector.svg" className=" w-5 h-5 me-[6px]"/>
+          {/* <img src="./Carbon/chatVector.svg" className=" w-5 h-5 me-[6px]"/> */}
+          <div className={`Carbon-icon-chatVector ${activeButton == rightText ? 'bg-white':'bg-[#8290a3]'}`}></div>
           {rightText}
         </Button>
       </div>
