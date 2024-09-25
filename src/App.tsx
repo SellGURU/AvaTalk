27e00b9913d0898ccd2ad63aa587dcb999e42454
amir <amirdehghani137722@gmail.com>
2024-09-25 18:05:30 +0330

    fix fonts

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   handleDivices()
   return (
     <>
-      <div className='w-full h-dvh flex overflow-hidden justify-center items-center '>
+      <div className='w-full h-dvh font-poppins flex overflow-hidden justify-center items-center '>
         <div className='w-full max-w-[32rem]'>
             <GoogleOAuthProvider clientId="750278697489-u68emmire3d35234obo1mne9v0eobmsu.apps.googleusercontent.com">
               <RouterProvider router={route} />
