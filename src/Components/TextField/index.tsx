b4d08f67e047fca569edab9d10209c4520fb04f9
amir <amirdehghani137722@gmail.com>
2024-09-25 12:14:32 +0330

    fix icon search box

--- src/Components/TextField/index.tsx
+++ src/Components/TextField/index.tsx
@@ -18,7 +18,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   setPhoneCountry?: (phone:PhoneCountry) => void;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onBlur: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  type: "text" | "password" | "email" | "phone";
+  type: "text" | "password" | "email" | "phone" | 'searchBox';
   inValid: boolean | string;
   errorMessage?: string;
   setValue?: (value:string) => void
@@ -143,6 +143,9 @@ const TextField: React.FC<InputProps> = ({
           onBlur={onBlur}
           value={value}
         />
+        {type == 'searchBox' &&
+          <img className={`Carbon-SearchBox-searchIcon `} src="../../../Carbon/search-normal.png" alt="" />
+        }
         {type === "password" && (
           <div
             data-testid="toggle-password"
