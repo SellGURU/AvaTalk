3640a33050ce7302ea461ff53806d3b6ab46441e
SmurfsNIma <sadeghinima090@gmail.com>
2024-10-08 10:13:09 +0330

    fix bug

--- src/Components/ChatItem/index.tsx
+++ src/Components/ChatItem/index.tsx
@@ -39,7 +39,7 @@ const ChatItem = ({data, theme,visibleDate,isLimitedChat}: { data: DataProps; th
               <div className={`${theme}-ChatItem-Vector`}></div>
             </div>
           </div>
-          <p className={`${theme}-ChatItem-content`}>{data.response.substring(0,57)+' ...'}</p>
+          <p className={`${theme}-ChatItem-content`}>{data.response.substring(0,35)+' ...'}</p>
         </div>
       </div>
     </div>
