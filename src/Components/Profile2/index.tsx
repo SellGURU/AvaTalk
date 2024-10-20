e0acf6d8cd9b28d038f3f5c3858be9a69fa3b825
SmurfsNIma <sadeghinima090@gmail.com>
2024-10-20 16:42:34 +0330

    v1.6.12

--- src/Components/Profile2/index.tsx
+++ src/Components/Profile2/index.tsx
@@ -706,7 +706,7 @@ const Profile2: React.FC<ProfileProps> = ({ theme }) => {
               ?
               <div className="w-full bg-white pb-4  flex justify-start items-center">
                 <img onClick={() => {window.open('https://portal.avatalk.me/#/')}} className=" cursor-pointer" src="/icons/avatalk.svg" alt="logo" />
-                <div className="text-[10px] ml-1 text-gray-700">Want your own <span onClick={() => {window.open('https://portal.avatalk.me/#/')}}  className="text-primary-color font-semibold cursor-pointer">Avatalk</span>? Create your AI Avatar in less than 3 minutes!</div>
+                <div className=" leading-[15px] font-normal text-[10px] ml-1 ">Want your own <span onClick={() => {window.open('https://portal.avatalk.me/#/')}}  className="text-primary-color font-medium cursor-pointer">Avatalk</span>? Create your AI Avatar in less than 3 minutes!</div>
               </div>
               // <div className=" flex px-5 py-6 flex-col gap-2 bg-white justify-start items-start text-xs w-full	">
               //     <img onClick={() => {window.open('https://portal.avatalk.me/#/')}} className="w-20 h-8 cursor-pointer" src="/Carbon/splashImage.svg" alt="logo" />
--- src/Pages/EditPages/File/index.tsx
+++ src/Pages/EditPages/File/index.tsx
@@ -2,7 +2,7 @@
 import { Button, TextField } from "symphony-ui";
 import { AccessNotifManager, BackIcon } from "../../../Components";
 import ImageUploadr from "../../../Components/UploadImage";
-import { FileBox,File } from "../../../Model";
+import { FileBox, File } from "../../../Model";
 import { useAuth } from "../../../hooks/useAuth";
 import { useFormik } from "formik";
 import * as Yup from "yup";
@@ -16,18 +16,24 @@ const validationSchema = Yup.object().shape({
 
 const EditFile = () => {
   const auth = useAuth();
-  const [limiteMdoe,setLimiteMode] = useState("defualt")
+  const [limiteMdoe, setLimiteMode] = useState("defualt");
   const navigate = useNavigate();
-  let currentBox = auth.currentUser.boxs.filter((item) => item.getTypeName() == "FileBox")[0] as FileBox;
+  let currentBox = auth.currentUser.boxs.filter(
+    (item) => item.getTypeName() == "FileBox"
+  )[0] as FileBox;
   if (currentBox == undefined) {
     currentBox = new FileBox("File", []);
   }
-  const [files,setFiles] = useState<Array<File>>(currentBox.getContents().map(((item:File) => Object.assign(new File('','','','0'),item))))
+  const [files, setFiles] = useState<Array<File>>(
+    currentBox
+      .getContents()
+      .map((item: File) => Object.assign(new File("", "", "", "0"), item))
+  );
   const initialValue = {
     title: currentBox.getTitle(),
     files: currentBox.getContents(),
   };
-  const [isReadyTO,setIsReadyTo] = useState(false)
+  const [isReadyTO, setIsReadyTo] = useState(false);
   const formik = useFormik({
     initialValues: initialValue,
     validationSchema,
@@ -36,27 +42,33 @@ const EditFile = () => {
     },
   });
   const submit = () => {
-    if((auth.currentUser.type_of_account.getType() == 'Free' && formik.values.files.length > 1) ||(auth.currentUser.type_of_account.getType() == 'Free' && limiteMdoe=='fileSize') ){
-      setIsReadyTo(true)
-    }else {
-      auth.currentUser.addBox(new FileBox(formik.values.title, formik.values.files));
+    if (
+      (auth.currentUser.type_of_account.getType() == "Free" &&
+        formik.values.files.length > 1) ||
+      (auth.currentUser.type_of_account.getType() == "Free" &&
+        limiteMdoe == "fileSize")
+    ) {
+      setIsReadyTo(true);
+    } else {
+      auth.currentUser.addBox(
+        new FileBox(formik.values.title, formik.values.files)
+      );
       navigate("/");
     }
   };
   useEffect(() => {
-    let numberofBiges = 0
+    let numberofBiges = 0;
     files.map((el) => {
-      if(Number(el.getSize()) > 10 * 1024 * 1024) {
-        numberofBiges = numberofBiges +1
+      if (Number(el.getSize()) > 10 * 1024 * 1024) {
+        numberofBiges = numberofBiges + 1;
       }
-    })
-    if(numberofBiges > 0){
-      setLimiteMode("fileSize")
-    }else {
-      setLimiteMode("defualt")
+    });
+    if (numberofBiges > 0) {
+      setLimiteMode("fileSize");
+    } else {
+      // setLimiteMode("defualt");
     }
-    
-  })
+  });
   return (
     <>
       <div className="absolute w-full hiddenScrollBar h-dvh overflow-scroll top-[0px] bg-white z-[15]">
@@ -65,13 +77,20 @@ const EditFile = () => {
         </div>
         <div className="mt-[120px] hiddenScrollBar h-full">
           <div className="px-6 mt-24  mb-[24px]">
-            <AccessNotifManager modeLimited={limiteMdoe} isLimited={isReadyTO} page="FileSetting"></AccessNotifManager>
-          </div>             
+            <AccessNotifManager
+              modeLimited={limiteMdoe}
+              isLimited={isReadyTO}
+              page="FileSetting"
+            ></AccessNotifManager>
+          </div>
           <div className=" px-6">
             <TextField
               {...formik.getFieldProps("title")}
               errorMessage={formik.errors?.title}
-              inValid={formik.errors?.title != undefined && (formik.touched?.title as boolean)}
+              inValid={
+                formik.errors?.title != undefined &&
+                (formik.touched?.title as boolean)
+              }
               theme="Carbon"
               label="Title"
               name="title"
@@ -86,37 +105,55 @@ const EditFile = () => {
                 return {
                   url: item.geturl(),
                   name: item.getName(),
-                  type:item.getType(),
-                  size:item.getSize()
+                  type: item.getType(),
+                  size: item.getSize(),
                 };
               })}
               limite={0}
+              onClick={(e) => {
+                if 
+                  (auth.currentUser.type_of_account.getType() == "Free" &&
+                    formik.values.files.length >= 1)
+                 
+                 {
+                  setLimiteMode("length");
+                  setIsReadyTo(true);
+
+                  e.preventDefault();
+                  e.stopPropagation();
+                }
+              }}
               userMode={auth.currentUser.type_of_account.getType()}
               uploades={(files: Array<any>) => {
-                console.log(files)
+                console.log(files);
                 const selectedFiles = Array.from(files);
                 const maxFileSize = 10 * 1024 * 1024; // 10 MB in bytes
                 // Filter files based on size (<= 10 MB)
-                const validFiles = selectedFiles.filter(file =>{
-                  if(file.size > maxFileSize ){
+                const validFiles = selectedFiles.filter((file) => {
+                  if (file.size > maxFileSize) {
                     // setIsReadyTo(true)
-                    setLimiteMode("fileSize")
+                    setLimiteMode("fileSize");
                   }
                   // if(auth.currentUser.type_of_account.getType()=='Free'){
                   //   return file.size <= maxFileSize
                   // }
-                  return true
-                } );                
-                console.log(validFiles)
-                if(validFiles.length > 1){
-                  setLimiteMode("length")
-                }
-                const converted:Array<File> = validFiles.map((item) => {
-                  console.log("item.type",item.type)
-                  const newFile:File = new File(item.url,item.name,item.type,item.size)
-                  return newFile
+                  return true;
                 });
-                setFiles(converted)
+                console.log(validFiles);
+                if (validFiles.length > 1) {
+                  setLimiteMode("length");
+                }
+                const converted: Array<File> = validFiles.map((item) => {
+                  console.log("item.type", item.type);
+                  const newFile: File = new File(
+                    item.url,
+                    item.name,
+                    item.type,
+                    item.size
+                  );
+                  return newFile;
+                });
+                setFiles(converted);
                 formik.setFieldValue("files", converted);
               }}
               mod="files"
@@ -129,13 +166,16 @@ const EditFile = () => {
             </Button>
           </div>
         </div>
-        {isReadyTO &&
+        {isReadyTO && (
           <div className="fixed w-full left-0 bottom-0 flex justify-center">
-            <ReadyForMore page="File" onClose={() => {
-              setIsReadyTo(false)
-            }} ></ReadyForMore>
+            <ReadyForMore
+              page="File"
+              onClose={() => {
+                setIsReadyTo(false);
+              }}
+            ></ReadyForMore>
           </div>
-        }         
+        )}
       </div>
     </>
   );
