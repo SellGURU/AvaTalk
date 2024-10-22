1de937901bccf1f27b68ac8e380f6900a77a862d
amir <amirdehghani137722@gmail.com>
2024-10-22 19:39:09 +0330

    fix schema data

--- src/Components/PersonSchema/index.tsx
+++ src/Components/PersonSchema/index.tsx
@@ -1,24 +1,52 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Helmet } from 'react-helmet';
-import { useAuth } from '../../hooks/useAuth';
+
+import Share from '../../Api/Share';
+import { useEffect, useState } from 'react';
 
 const PersonSchema = () => {
-  const context = useAuth()
-  const socialBOx:any = context.currentUser.boxs.filter(el => el.getTypeName() == 'SocialBox')[0]
-  const schemaData = {
+
+  const [myData,setMydata] = useState<any>(null)
+  useEffect(() => {
+    Share.getShareData('/presentation_info/user='+window.location.hash.split("/")[2],(data) => {
+      setMydata(data)
+    })    
+  },[])
+
+
+  // const socialBOx:any = myData.boxs.filter((el:any) => el.typeName == 'SocialBox')[0]
+  const [schemaData,setScemaData] =  useState({
     "@context": "https://schema.org/",
     "@type": "Person",
-    "name": context.currentUser.information?.firstName+""+context.currentUser.information?.lastName,
-    "url": "https://portal.avatalk.me/#/A/"+context.currentUser.information?.unique_id,
-    "image": context.currentUser.information?.imageurl,
-    "sameAs":socialBOx?.getSocialMedias().map((el:any) => el.value),
-    "jobTitle":context.currentUser.information?.job,
+    "name": myData?.information.first_name+""+myData?.information.last_name,
+    "url": "https://portal.avatalk.me/#/A/"+'',
+    "image":'',
+    // "sameAs":socialBOx?.getSocialMedias().map((el:any) => el.value),
+    "jobTitle":'',
     "worksFor": {
       "@type": "Organization",
-      "name": context.currentUser.information?.company
+      "name": ''
     }
-  };
+  });
+  useEffect(() => {
+    console.log(myData)
+    if(myData!= undefined &&  myData!= null){
+      setScemaData({
+      "@context": "https://schema.org/",
+      "@type": "Person",
+      "name": myData?.information.first_name+""+myData?.information.last_name,
+      "url": "https://portal.avatalk.me/#/A/"+'',
+      "image":'',
+      // "sameAs":socialBOx?.getSocialMedias().map((el:any) => el.value),
+      "jobTitle":'',
+      "worksFor": {
+        "@type": "Organization",
+        "name": ''
+      }
+    })
 
+    }
+  },[myData])
   return (
     <Helmet>
       <script type="application/ld+json">
