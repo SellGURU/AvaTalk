a9152fc9a21c9caf61b95ee080cbe29dee8a8926
SmurfsNIma <sadeghinima090@gmail.com>
2024-10-23 12:25:12 +0330

    fix build issue

--- src/Pages/ChangePassword/ChangePassword.tsx
+++ src/Pages/ChangePassword/ChangePassword.tsx
@@ -7,7 +7,6 @@ import { useState , useEffect } from "react";
 import * as Yup from "yup";
 import { SuccessModal } from "./SucessModal";
 import { Auth } from "../../Api";
-import { toast } from "react-toastify";
 const ChangePassowrd = () => {
   const navigate = useNavigate();
 
