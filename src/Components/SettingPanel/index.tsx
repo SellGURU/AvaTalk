bcfc106735af0aa76cd92a416204b2f2737e0223
SmurfsNIma <sadeghinima090@gmail.com>
2024-10-29 16:11:19 +0330

    fix setting scroll

--- src/Components/SettingPanel/index.tsx
+++ src/Components/SettingPanel/index.tsx
@@ -103,10 +103,10 @@ const SettingPanel: React.FC<SettingPanelProps> = ({theme}) => {
   return (
     <>
     
-      <div className={`Carbon-ContactsView-Container  `}>
+      <div className={`Carbon-ContactsView-Container overflow-auto  `}>
         <Outlet></Outlet>
         <p className={`${theme}-Edit-title px-6 pb-[20px]`}>Setting</p>
-        <div className="px-6 mt-0   ">
+        <div className="px-6  ">
           {isShowPlanCard &&
             <div className="">
               <PlanCard onClose={() => {setIsShowPalnCard(false)}}></PlanCard>
