a2498d2c9b5793ce9fc133a1184cd49cabc5b4b8
amir <amirdehghani137722@gmail.com>
2024-11-04 13:03:09 +0330

    fix google map issues

--- src/Model/GoogleMap/index.tsx
+++ src/Model/GoogleMap/index.tsx
@@ -1,9 +1,10 @@
 // import LocationPicker, { PointMode } from "react-leaflet-location-picker";
 import { Box } from "..";
 // import { LatLngTuple } from "leaflet";
-import LeafletMapLoader, { Init } from "../../Components/Base/LefeatMapLoader";
-import { createRef } from "react";
-import { LeafletElement } from "../../Components/Base/LefeatMapLoader/leaflet-map-loader";
+// import LeafletMapLoader, { Init } from "../../Components/Base/LefeatMapLoader";
+// import { createRef } from "react";
+// import { LeafletElement } from "../../Components/Base/LefeatMapLoader/leaflet-map-loader";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 
 interface Location {
     lat:number
@@ -16,89 +17,73 @@ class GoogleMapBox extends Box {
         this.type_name = 'GoogleMapBox'
         this.order = 5
     }
-    public resolveRender(theme: string,mode:string): JSX.Element {
-        const mapRef = createRef<LeafletElement>();
+    public resolveRender(theme: string): JSX.Element {
+        // const mapRef = createRef<LeafletElement>();
         // eslint-disable-next-line react-hooks/rules-of-hooks
-        console.log(mode)    
-        const init: Init = async (initializeMap) => {
-        const mapMarkerSvg =
-            window.btoa(`<svg xmlns="http://www.w3.org/2000/svg" width="20" height="31.063" viewBox="0 0 20 31.063">
-                    <g id="Group_16" data-name="Group 16" transform="translate(-408 -150.001)">
-                    <path id="Subtraction_21" data-name="Subtraction 21" d="M10,31.064h0L1.462,15.208A10,10,0,1,1,20,10a9.9,9.9,0,0,1-1.078,4.522l-.056.108c-.037.071-.077.146-.121.223L10,31.062ZM10,2a8,8,0,1,0,8,8,8,8,0,0,0-8-8Z" transform="translate(408 150)" fill="#eb2617"/>
-                    <circle id="Ellipse_26" data-name="Ellipse 26" cx="6" cy="6" r="6" transform="translate(412 154)" fill="#eb2617"/>
-                    </g>
-                </svg>
-                `);            
-            const mapInstance =await initializeMap({
-            config: {
-                center:[this.location.lan,this.location.lat],
-                zoom:14,
-            },
-            });    
-            if (mapInstance) {
-            const apiKey = "1e86fd5a7f60486a8e899411776f60d5";
-            const { map, leaflet } = mapInstance;
-
-            leaflet
-                .tileLayer(
-                `https://tile.thunderforest.com/atlas/{z}/{x}/{y}@2x.png?apikey=${apiKey}`,
-                {
-                    attribution:
-                    "Map data &copy; OpenStreetMap contributors, Tiles &copy; Thunderforest",
-                }
-                )
-                .addTo(map);
-
-            const marker = leaflet.marker(
-            {
-                lat:this.location.lan,
-                lng:this.location.lat,
-            },
-            {
-                title: 'location',
-                attribution:'',
-                icon: leaflet.icon({
-                iconUrl: `data:image/svg+xml;base64,${mapMarkerSvg}`,
-                iconAnchor: leaflet.point(10, 35),
-                }),
-            }
-            );    
-            map.addLayer(marker);                             
-            }           
-        }       
-        // const pointVals = [[this.location.lan, this.location.lat]];
-        // const pointMode:PointMode = {
-        //     banner: false,
-        //     control: {
-        //         values: pointVals as LatLngTuple[],
+        // console.log(mode)    
+        // const init: Init = async (initializeMap) => {
+        // const mapMarkerSvg =
+        //     window.btoa(`<svg xmlns="http://www.w3.org/2000/svg" width="20" height="31.063" viewBox="0 0 20 31.063">
+        //             <g id="Group_16" data-name="Group 16" transform="translate(-408 -150.001)">
+        //             <path id="Subtraction_21" data-name="Subtraction 21" d="M10,31.064h0L1.462,15.208A10,10,0,1,1,20,10a9.9,9.9,0,0,1-1.078,4.522l-.056.108c-.037.071-.077.146-.121.223L10,31.062ZM10,2a8,8,0,1,0,8,8,8,8,0,0,0-8-8Z" transform="translate(408 150)" fill="#eb2617"/>
+        //             <circle id="Ellipse_26" data-name="Ellipse 26" cx="6" cy="6" r="6" transform="translate(412 154)" fill="#eb2617"/>
+        //             </g>
+        //         </svg>
+        //         `);            
+        //     const mapInstance =await initializeMap({
+        //     config: {
+        //         center:[this.location.lan,this.location.lat],
+        //         zoom:14,
         //     },
-            
-        // };         
+        //     });    
+        //     if (mapInstance) {
+        //     const apiKey = "AIzaSyB0NfYbn1RS0UvUX8HE40l_uykzQXYy6d8";
+        //     const { map, leaflet } = mapInstance;
+
+        //     leaflet
+        //         .tileLayer(
+        //         `https://tile.thunderforest.com/atlas/{z}/{x}/{y}@2x.png?apikey=${apiKey}`,
+        //         {
+        //             attribution:
+        //             "Map data &copy; OpenStreetMap contributors, Tiles &copy; Thunderforest",
+        //         }
+        //         )
+        //         .addTo(map);
+
+        //     const marker = leaflet.marker(
+        //     {
+        //         lat:this.location.lan,
+        //         lng:this.location.lat,
+        //     },
+        //     {
+        //         title: 'location',
+        //         attribution:'',
+        //         icon: leaflet.icon({
+        //         iconUrl: `data:image/svg+xml;base64,${mapMarkerSvg}`,
+        //         iconAnchor: leaflet.point(10, 35),
+        //         }),
+        //     }
+        //     );    
+        //     map.addLayer(marker);                             
+        //     }           
+        // }       
+ 
         return (
             <div className={`${theme}-Profile-Vectors justify-center relative`}>
-              
-                <div className="px-0 mt-3 w-full">
-                    {/* <LocationPicker  showInputs={false} pointMode={pointMode} mapStyle={{ height: "211px", borderRadius: "27px",zIndex:0 }}  /> */}
-                    <LeafletMapLoader
-                        mapRef={mapRef}
-                        init={init}
-                        draggable={false}
-                        className="h-[210px] z-[5]  rounded-md bg-slate-200"
-                    />  
-                    {/* <div className="w-full absolute z-[5] bottom-8 flex justify-center">
-                        <div className=" w-[281px] flex justify-start px-4 items-center borderBox-Gray boxShadow-Gray h-[51px] bg-[#F3F4F6] rounded-[27px]">
-                            <div className="borderBox-Gray flex justify-center items-center boxShadow-Gray w-[37px] h-[37px] rounded-[5px]">
-                                <img src="./Carbon/map.svg" alt="" />
-                            </div>
-                            <div onClick={() => {
-                                window.open(`https://www.google.com/maps/@${this.location.lan},${this.location.lat}16z?entry=ttu`)
-                            }} className="ml-6">
-                                <div className="text-[#94A3B8] text-sm">Current Location</div>
-                            </div>
-                        </div>  
-                    
-                    </div>          */}
-                </div>            
+                {this.location &&
+                    <div className="px-0 mt-3 w-full relative z-0">
+                                <MapContainer dragging={false}  center={[this.location?.lan, this.location?.lat]} zoom={13} style={{ height: '250px', borderRadius: '27px' }}>
+                                    <TileLayer
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                    />
+                                    <Marker position={[this.location?.lan, this.location?.lat]} />
+                                    
+                                </MapContainer>
+
+
+                    </div>            
+                }
             </div>            
         )
     }
