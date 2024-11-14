2946a7f4c2142e26bf480792759e4b7770709280
amir <amirdehghani137722@gmail.com>
2024-11-14 15:59:10 +0330

    fix gallery

--- src/Model/GalleryBox/index.tsx
+++ src/Model/GalleryBox/index.tsx
@@ -68,6 +68,7 @@ class GalleryBox extends Box{
                         if(!isFullscreen){
                             document.querySelectorAll(".image-gallery-image").forEach((element:any) => {
                                 console.log(element)
+                                element.style.height = "400px";
                                 element.style.maxHeight = "400px "; // Change property and value as needed
                             });                            
                         }
@@ -78,6 +79,7 @@ class GalleryBox extends Box{
                                 // const cols =document.getElementsByClassName("image-gallery-image")
                                 document.querySelectorAll(".image-gallery-image").forEach((element:any) => {
                                     console.log(element)
+                                    element.style.height = "100vh ";
                                     element.style.maxHeight = "100vh "; // Change property and value as needed
                                 });
                                 onClick(e)
