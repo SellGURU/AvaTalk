0405f06e999da76356f08a30cba026566d6e056e
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-12-12 13:10:55 +0330

    UPDATE:add pwa icon

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,66 @@ export default defineConfig({
           devOptions: {
               enabled: true
           },
+          includeAssets: ['favicon.ico', "icon-512x512.png","icon-384x384.png","icon-256x256.png","icon-192x192.png","icon-152x152.png",'icon-48x48.png', 'icon-72x72.png', "icon-96x96.png","icon-128x128.png","icon-144x144.png"],
+          manifest: {
+              name: 'Avatalk',
+              short_name: 'Avatalk',
+              description: 'Avatalk',
+              theme_color: '#ffffff',
+              display: "standalone",
+              icons: [
+                  {
+                      "src": "/iconspwa/icon-48x48.png",
+                      "sizes": "48x48",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/iconspwa/icon-72x72.png",
+                      "sizes": "72x72",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/iconspwa/icon-96x96.png",
+                      "sizes": "96x96",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/iconspwa/icon-128x128.png",
+                      "sizes": "128x128",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/iconspwa/icon-144x144.png",
+                      "sizes": "144x144",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/iconspwa/icon-152x152.png",
+                      "sizes": "152x152",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/iconspwa/icon-192x192.png",
+                      "sizes": "192x192",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/iconspwa/icon-256x256.png",
+                      "sizes": "256x256",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/iconspwa/icon-384x384.png",
+                      "sizes": "384x384",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/iconspwa/icon-512x512.png",
+                      "sizes": "512x512",
+                      "type": "image/png"
+                  }
+              ],
+          },
           workbox: {
               maximumFileSizeToCacheInBytes: 3000000
           }
