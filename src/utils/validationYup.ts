c35e6cf136780dec2b2fa24e059406b3fdf2b8ef
amir <amirdehghani137722@gmail.com>
2025-02-02 11:48:21 +0330

    update validation text

--- src/utils/validationYup.ts
+++ src/utils/validationYup.ts
@@ -3,9 +3,9 @@ import * as Yup from "yup";
 const validationYup = (property:string) => {
     switch(property) {
         case 'company':
-            return Yup.string().min(3,'Company name must be between 3 and 15 characters.').max(15,'Company name must be between 3 and 15 characters.').trim('Company Name must not be empty or just spaces.')
+            return Yup.string().min(3,'Company name must be between 3 to 15 characters.').max(15,'Company name must be between 3 and 15 characters.').trim('Company Name must not be empty or just spaces.')
         case 'job':
-            return Yup.string().min(3,'Job title must be between 3 and 15 characters.').max(15,'Job title must be between 3 and 15 characters.').trim('Job must not be empty or just spaces.')
+            return Yup.string().min(3,'Job title must be between 3 to 15 characters.').max(15,'Job title must be between 3 and 15 characters.').trim('Job must not be empty or just spaces.')
         case 'fullName':
             return Yup.string().required('Full Name is required').max(25,'Full Name must be between 5 and 25 characters.').min(5,'Full Name must be between 5 and 25 characters.').trim('Full Name must not be empty or just spaces.')
         case 'firstname':
