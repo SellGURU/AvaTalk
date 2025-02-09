00b63e7640b9f726c091724268a23aabac4b6c0c
amir <amirdehghani137722@gmail.com>
2025-02-09 08:55:05 +0330

    update detail deveop

--- src/Pages/SignUp/onboardingSteps/steps/BusinessStep.tsx
+++ src/Pages/SignUp/onboardingSteps/steps/BusinessStep.tsx
@@ -132,7 +132,7 @@ const BusinessStep:React.FC<BusinessStepProps> = ({
                             onSubmit()           
                         }).catch((err) => {
                             console.log(err)
-                           formik.setFieldError("phone",err.response.data)
+                           formik.setFieldError("phone",err.response.data.detail)
                         })
                     }}  theme="Carbon">Continue</Button>
                 </div>                                                         
