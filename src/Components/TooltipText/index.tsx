52c726ba45d4f6367ed63ebf62816f4ba4794fbd
amir <amirdehghani137722@gmail.com>
2025-03-04 12:55:52 +0330

    change tooltips

--- src/Components/TooltipText/index.tsx
+++ src/Components/TooltipText/index.tsx
@@ -1,5 +1,7 @@
-import { Tooltip } from 'react-tooltip';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// import { Tooltip } from 'react-tooltip';
 import { ReactElement, useEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
 
 interface TooltipTextProps extends React.ButtonHTMLAttributes<HTMLDivElement> {
   tooltipValue: string;
@@ -11,8 +13,21 @@ const TooltipText: React.FC<TooltipTextProps> = ({
   children,
   ...props
 }) => {
-  const textRef = useRef(null);
+  const textRef = useRef<any>(null);
   const [isEllipsized, setIsEllipsized] = useState(true);
+  const [visible, setVisible] = useState(false);
+  const [position, setPosition] = useState<{ top: number; left: number }>({
+    top: 0,
+    left: 0,
+  });
+  // const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (visible && textRef.current) {
+      const rect = textRef.current.getBoundingClientRect();
+      setPosition({ top: rect.top - 30, left: rect.left + rect.width / 2 });
+    }
+  }, [visible]);  
   useEffect(() => {
     if (textRef.current) {
       const { offsetWidth, scrollWidth } = textRef.current;
@@ -25,6 +40,8 @@ const TooltipText: React.FC<TooltipTextProps> = ({
         ref={textRef}
         data-tooltip-id={isEllipsized ? "tooltip"+tooltipValue : ''}
         data-tooltip-content={tooltipValue+""}
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
         {...props}
         style={{
           textWrap: 'nowrap',
@@ -34,7 +51,30 @@ const TooltipText: React.FC<TooltipTextProps> = ({
       >
         {children}
       </span>
-      {isEllipsized && <Tooltip variant='light'  positionStrategy='fixed' style={{width:'300px',wordBreak:'break-word',zIndex:'9999 !important'}} id={"tooltip"+tooltipValue}></Tooltip>}
+      {/* {isEllipsized && <Tooltip variant='light'  positionStrategy='fixed' style={{width:'300px',wordBreak:'break-word',zIndex:'9999 !important'}} id={"tooltip"+tooltipValue}></Tooltip>} */}
+        {isEllipsized && visible && createPortal(
+          <div
+          className='shadow-sm'
+            style={{
+              position: "fixed",
+              top: `${position.top}px`,
+              left: `${position.left}px`,
+              transform: "translateX(-50%)",
+              backgroundColor: "white",
+              color: "black",
+              padding: "5px 10px",
+              maxWidth:'60%',
+              borderRadius: "5px",
+              whiteSpace: "nowrap",
+              zIndex: 99999, // Ensures tooltip appears above all layers
+            }}
+          >
+            <>
+              {tooltipValue}
+            </>
+          </div>,
+          document.body
+        )}
     </>
   );
 };
