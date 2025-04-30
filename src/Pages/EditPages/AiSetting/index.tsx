7a0750e0bf44dae7d15da6aeab3f76619c940580
amir <amirdehghani137722@gmail.com>
2025-04-30 10:13:40 +0330

    Enhance LocationModal to fetch address on mount, adjust GoogleMap box shadow, and update video source URL

--- src/Pages/EditPages/AiSetting/index.tsx
+++ src/Pages/EditPages/AiSetting/index.tsx
@@ -474,7 +474,7 @@ const EditAiSetting = () => {
             }} className="text-[#06B6D4] cursor-pointer">tutorial video</span> for a step-by-step guide.</div>
           </div>
 
-          <video onEnded={handleVideoEnd}  ref={videoRef} src={'https://codieblob.blob.core.windows.net/avatalk/Videos/tut_1.mp4'} style={{display:isVisible?'block':'none'}} />
+          <video onEnded={handleVideoEnd}  ref={videoRef} src={'https://newcodieblob.blob.core.windows.net/avatalk/Videos/tut_1.mp4'} style={{display:isVisible?'block':'none'}} />
 
         </div>
         {isReadyTO && (
